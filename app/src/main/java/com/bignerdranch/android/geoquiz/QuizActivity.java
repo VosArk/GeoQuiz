9139622734fbb62bd05beea77f4194138b025e3c
VosArk <vosark@yandex.ru>
2018-01-08 00:42:33 +0300

    added horizontal orientation

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,14 +2,22 @@ package com.bignerdranch.android.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class QuizActivity extends AppCompatActivity {
 
+    private final static String TAG = "QuizActivity";
+    private final static String KEY_INDEX = "index";
+    private final static String KEY_ANSWERED_QUESTIONS = "questions";
+    private final static String KEY_CORRECT_ANSWERS = "correct";
+    private final static String KEY_INCORRECT_ANSWERS = "incorrect";
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
@@ -25,11 +33,22 @@ public class QuizActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    private int mCorrectAnswersCount = 0;
+    private int mIncorrectAnswersCount = 0;
+    private ArrayList<Integer> mAnsweredQuestions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mCorrectAnswersCount = savedInstanceState.getInt(KEY_CORRECT_ANSWERS, 0);
+            mIncorrectAnswersCount = savedInstanceState.getInt(KEY_INCORRECT_ANSWERS, 0);
+            mAnsweredQuestions = savedInstanceState.getIntegerArrayList(KEY_ANSWERED_QUESTIONS);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view);
         updateQuestion();
@@ -67,32 +86,71 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
 
-        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putInt(KEY_CORRECT_ANSWERS, mCorrectAnswersCount);
+        savedInstanceState.putInt(KEY_INCORRECT_ANSWERS, mIncorrectAnswersCount);
+        savedInstanceState.putIntegerArrayList(KEY_ANSWERED_QUESTIONS, mAnsweredQuestions);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        if (mCorrectAnswersCount + mIncorrectAnswersCount != mQuestionBank.length) {
+            int question = mQuestionBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+        } else {
+            mQuestionTextView.setText(String.format(getString(R.string.correct_results_text), mCorrectAnswersCount, mQuestionBank.length));
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId;
 
-        int messageResId = 0;
-
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-        } else {
-            messageResId = R.string.incorrect_toast;
+        if (!mAnsweredQuestions.contains(mCurrentIndex)) {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+                mCorrectAnswersCount++;
+            } else {
+                messageResId = R.string.incorrect_toast;
+                mIncorrectAnswersCount++;
+            }
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+            mAnsweredQuestions.add(mCurrentIndex);
         }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
